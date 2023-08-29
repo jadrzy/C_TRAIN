79dd231e6569b49d13a56dace75f608a662b1949
Jan <jdrzyzga10@gmail.com>
2023-08-29 12:06:08 +0200

    ex13

--- CHAPTER.13/ch13_ex13.c
+++ CHAPTER.13/ch13_ex13.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h> // for exit()
-#include <string.h>
-#define ROW 20
-#define COL 30
+#include <string.h> // for strlen()
+#include <ctype.h> // for isnum()
 
 int main(void){
     char array_of_symbols[10] = {" .~:=!*#$@"};
-    int number;
-    char test[1000];
-    int row, col;
     FILE * fp;
     if ((fp = fopen("picture.txt", "r")) == NULL){
         printf("Error opening file picture.txt\n");
         exit(EXIT_FAILURE);
     }
-    // checking size of a file
-    fseek(fp, 0, SEEK_END);                           
-    long int size = ftell(fp);                                 
-    fseek(fp, 0, SEEK_SET);  
+    // calculating dimensions of array
+    int size = 0,
+        row = 0,
+        col = 0,
+        number;
+    char test[1000];
+    while (fscanf(fp, "%d", &number) == 1)
+        size++;
+    rewind(fp);
     fgets(test, 1000, fp);
-    while (fscanf(%d,))
-    col = strlen(test);
+    for (int i = 0; i < strlen(test); i++)
+        if (isdigit(test[i]) != 0)
+            col++;
     row = size / col;
-    printf("col: %d row: %d", col, row);
-
-    int array[ROW][COL + 1];    
+    // implementing VLA
+    int array[row][col + 1];
 
     int row_count = 0;
     int col_count = 0;
     while (fscanf(fp, "%d", &number) == 1){                         // scanning numbers from file
         array[row_count][col_count] = array_of_symbols[number];     // converting numbers to symbols and saving results in the array
         col_count++;
-        if (col_count == COL){
+        if (col_count == col){
             col_count = 0;
             row_count++;
         }
@@ -41,8 +42,8 @@ int main(void){
         printf("Error opening file picture.txt\n");
         exit(EXIT_FAILURE);
     }
-    for (int i = 0; i < ROW; i++){                                  // printing to the file
-        for (int j = 0; j < COL; j++)
+    for (int i = 0; i < row; i++){                                  // printing to the file
+        for (int j = 0; j < col; j++)
             fprintf(fp, "%c ", array[i][j]);
         fprintf(fp, "\n");
     }
