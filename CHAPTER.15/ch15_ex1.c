3904bf35a6aaf05b0488c1f4f8ef0b09ed9b9d9b
Jan <jdrzyzga10@gmail.com>
2023-10-03 21:03:43 +0200

    CH15 start

---
+++ CHAPTER.15/ch15_ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int b2v(char *ps, int n);
+
+int main(void){
+    char string[9] = {"011"};
+    printf("%s in decimal is %d\n", string, b2v(string, strlen(string)));
+    return 0;
+}
+
+int b2v(char * ps, int n){
+    int value = 0;
+    int power = 0;
+    for (int i = n - 1; i > 0; i--, power++){
+        if (ps[i] == '1'){
+            value += pow(2, power); 
+        }
+    }
+    return value;
+}
